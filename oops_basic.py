b2714b07f9e5d993b587524be6043d8c5fb92f1b
Kailash Senthil <kaisen1198@gmail.com>
2025-06-26 16:15:23 +0530

    Pushing the code to main branch.

--- oops_basic.py
+++ oops_basic.py
@@ -1,2 +1,16 @@
-print("Hello World");
-print("Hello");
+
+# This is the entire block of core parts on OOPS related to Class, methods and Object
+
+class cricket:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def team(self):
+        print("CSK")
+    
+    def add_two(self, x):
+        return x + 2
+
+c = cricket("Dhoni", 43)
+print(c.add_two(c.age))
