f5dfc9d453d53e617d9e4529177be21201a8513c
Kailash Senthil <kaisen1198@gmail.com>
2025-06-26 16:18:47 +0530

    Committed with the changes

--- oops_basic.py
+++ oops_basic.py
@@ -1,16 +1,16 @@
 
 # This is the entire block of core parts on OOPS related to Class, methods and Object
 
-class cricket:
-    def __init__(self, name, age):
+class cricket:                                  # Class is the blueprint for creating objects
+    def __init__(self, name, age):              # __init__ is the constructor method and runs automatically when the object is created
         self.name = name
-        self.age = age
+        self.age = age                          # Why should we use 'self' is because the object needs to access the data present in the method. If not, it won't know that you are referring to the object's own
     
-    def team(self):
+    def team(self):                             # Method 2 - team()
         print("CSK")
     
-    def add_two(self, x):
+    def add_two(self, x):                       # Method 3 - add_two(x)
         return x + 2
 
-c = cricket("Dhoni", 43)
-print(c.add_two(c.age))
+c = cricket("Dhoni", 43)                        # Object - c
+print(c.add_two(c.age))                        # Calling a method and print the output
