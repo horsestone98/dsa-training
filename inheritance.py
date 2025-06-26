378d4b83e0369443085e84f03bd7b588791592fe
Kailash Senthil <kaisen1198@gmail.com>
2025-06-26 17:52:58 +0530

    Inheritance notebook

---
+++ inheritance.py
@@ -0,0 +1,20 @@
+class Pet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def show(self):
+        print(f"I am {self.name} and am {self.age} years old")
+    def speak(self):
+        print(f"I do not speak in the language you guys do")
+
+class Doggy(Pet):
+    def __init__(self, name, age, sound):
+        super().__init__(name, age)
+        self.sound = sound
+    def voice(self):
+        print(f"I am {self.name}, {self.age} years old and I {self.sound}")
+
+p = Pet("Shadow", 4)
+p.show()
+d = Doggy("Shadow", 4, "bark")
+d.voice()
